6803e344f1e0bd1aef3409d75feee21be46ec3ff
ScottDenton <scottyd_14@hotmail.com>
2019-06-13 19:58:24 -0700

    Fib

--- nib.rb
+++ nib.rb
@@ -6,12 +6,14 @@
 
 
 def fib(l,n)
+  # puts l
+  # puts n
   return 0 if n == 0
   return 1 if n <= l
   # return fib(n-1) + fib(n-2)
   total = 0
-  (1..l).each do |i|
-    total += (fib(l, n-i))
+  (1..(l.to_i)).each do |i|
+    total += (fib((l.to_i), (n.to_i) - i.to_i))
   end
   total
 end
@@ -21,7 +23,10 @@ end
 
 lines=ARGF.read.split("\n")
 lines.each do |line|
+  # puts line
   l = line.split(' ')[0]
+  # puts l
   n = line.split(' ')[1]
+  # puts n
   puts fib(l,n)
 end
